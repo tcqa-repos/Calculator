acbc47d09351aaabb00b9887362105203aefa7f1
Julia Reshetnikova <julia.reshetnikova@jetbrains.com>
2018-04-05 19:21:02 +0300

    check for new inspetion

--- src/pack/MySortedMap.java
+++ src/pack/MySortedMap.java
@@ -2,6 +2,8 @@ package pack;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.SortedMap;
 
@@ -10,6 +12,12 @@ public abstract class MySortedMap implements SortedMap {
     @Nullable
     @Override
     public Comparator comparator() {
+        Collections.sort(Arrays.asList(new Object[]{"aaa"}), new Comparator<Object>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+        });
         return null;
     }
 }
